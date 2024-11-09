b6484c2cd7bf2e870cc6ec35589fb88b9d19cdd3
Arunponugoti <arun.devop1@gmail.com>
2024-11-09 15:10:53 +0530

    actions

--- 03-firewalls.tf
+++ 03-firewalls.tf
@@ -1,30 +1,30 @@
-# Firwall Rule: SSH
-resource "google_compute_firewall" "myfirewall_ssh_label" {
-    name = "ssh-namefirewall"
-    allow {
-      ports = ["22"]
-      protocol = "tcp"
+# # Firwall Rule: SSH
+# resource "google_compute_firewall" "myfirewall_ssh_label" {
+#     name = "ssh-namefirewall"
+#     allow {
+#       ports = ["22"]
+#       protocol = "tcp"
 
-    }
-    direction = "INGRESS"
-    network = google_compute_network.my-vpc-label.id 
-    priority = 1000
-    source_ranges = [ "0.0.0.0/0" ]
-    target_tags = [ "ssh-tags" ]
- }
+#     }
+#     direction = "INGRESS"
+#     network = google_compute_network.my-vpc-label.id 
+#     priority = 1000
+#     source_ranges = [ "0.0.0.0/0" ]
+#     target_tags = [ "ssh-tags" ]
+#  }
 
- # Firewall rule: HTTP port 80
+#  # Firewall rule: HTTP port 80
 
-resource "google_compute_firewall" "firewall_http_label" {
-    name = "firewall-http-name"
-    allow {
-      ports = [ "80" ]
-      protocol = "tcp"
-    }
-    direction = "INGRESS"
-    network = google_compute_network.my-vpc-label.id 
-    priority = 1000
-    source_ranges = [ "0.0.0.0/0" ]
-    target_tags = [ "http-tags" ]
+# resource "google_compute_firewall" "firewall_http_label" {
+#     name = "firewall-http-name"
+#     allow {
+#       ports = [ "80" ]
+#       protocol = "tcp"
+#     }
+#     direction = "INGRESS"
+#     network = google_compute_network.my-vpc-label.id 
+#     priority = 1000
+#     source_ranges = [ "0.0.0.0/0" ]
+#     target_tags = [ "http-tags" ]
   
-}
+# }
