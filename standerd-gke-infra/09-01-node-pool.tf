28c7c38b8f1424e772fbd405a05cbf64674bd0a7
Arunponugoti <arun.devop1@gmail.com>
2024-11-21 13:05:04 +0530

    changes

--- standerd-gke-infra/09-01-node-pool.tf
+++ standerd-gke-infra/09-01-node-pool.tf
@@ -1,17 +1,17 @@
-# # Resource: GKE Node Pool 1
-# resource "google_container_node_pool" "nodepool_1" {
-#   name       = "${local.name}-node-pool-1"
-#   location   = "us-east1-b"
-#   cluster    = google_container_cluster.gke_cluster.name
-#   node_count = 1
-#   node_config {
-#     preemptible  = true
-#     machine_type = var.machine_type
-#     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-#     service_account = "githubactions-sa@turnkey-guild-441104-f3.iam.gserviceaccount.com"
-#     oauth_scopes    = [
-#       "https://www.googleapis.com/auth/cloud-platform"
-#     ]
-#     tags = [tolist(google_compute_firewall.fw_ssh.target_tags)[0]]
-#   }
-# }
+# Resource: GKE Node Pool 1
+resource "google_container_node_pool" "nodepool_1" {
+  name       = "${local.name}-node-pool-1"
+  location   = "us-east1-b"
+  cluster    = google_container_cluster.gke_cluster.name
+  node_count = 1
+  node_config {
+    preemptible  = true
+    machine_type = var.machine_type
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = "githubactions-sa@turnkey-guild-441104-f3.iam.gserviceaccount.com"
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+    tags = [tolist(google_compute_firewall.fw_ssh.target_tags)[0]]
+  }
+}
