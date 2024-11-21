6d762435a995755e19ad5c607ef7f9c3d01da517
Arunponugoti <arun.devop1@gmail.com>
2024-11-21 07:58:35 +0530

    adding

---
+++ standerd-gke-infra/00-version.tf
@@ -0,0 +1,24 @@
+# provide the required versions 
+
+# terraform gcp provider block
+provider "google" {
+  region = var.gcp_region1
+  project = var.gcp_project
+}
+
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.8"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = ">= 5.32.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "terraform-statefile-bucket-tf"
+    prefix = "dev-env/gke-cluster-public"    
+  }
+}
+
+
