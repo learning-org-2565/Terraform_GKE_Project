d763b109a93ff43376ff1d8826a4663efb0503c3
Arunponugoti <arun.devop1@gmail.com>
2024-11-24 21:46:43 +0530

    private cluster

--- 03-gke_private_cluster/08-node-pool.tf
+++ 03-gke_private_cluster/08-node-pool.tf
@@ -3,7 +3,7 @@
 
 resource "google_container_node_pool" "private_gke_nodepool" {
   name = "${local.name}-nodepool"
-  location = "us-east1-c"
+  location = "us-central1"
   cluster = google_container_cluster.private_gke_cluster.name 
   initial_node_count = 1
   autoscaling {
