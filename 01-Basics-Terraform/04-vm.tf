6835db5b5178c16b11f4f61c5b1196d6383342c4
Arunponugoti <arun.devop1@gmail.com>
2024-12-10 12:39:21 +0530

    added github actions code

--- 01-Basics-Terraform/04-vm.tf
+++ 01-Basics-Terraform/04-vm.tf
@@ -1,19 +1,19 @@
-# # Resource Virtual machine
-# resource "google_compute_instance" "myvm-label" {
-#     name = "my-vm-name"
-#     machine_type = var.gcp_machine_type
-#     zone = var.gcp_zone
-#     boot_disk {
-#       initialize_params {
-#         image = var.boot_disk_image_name
-#       }
-#     }
-#     network_interface {
-#         subnetwork = google_compute_subnetwork.Subnet-label.id
-#       access_config {
+# Resource Virtual machine
+resource "google_compute_instance" "myvm-label" {
+    name = "my-vm-name"
+    machine_type = var.gcp_machine_type
+    zone = var.gcp_zone
+    boot_disk {
+      initialize_params {
+        image = var.boot_disk_image_name
+      }
+    }
+    network_interface {
+        subnetwork = google_compute_subnetwork.Subnet-label.id
+      access_config {
         
-#       }
-#     }
+      }
+    }
 
   
-# }
+}
--- 01-Basics-Terraform/05-output-values.tf
+++ 01-Basics-Terraform/05-output-values.tf
@@ -1,19 +1,19 @@
-# # outputs 
+# outputs 
 
-# #Attribites
+#Attribites
 
-# output "instance-id" {
-#   description = "to see the instance id in the command line "
-#   value = google_compute_instance.myvm-label.instance_id
-# }
+output "instance-id" {
+  description = "to see the instance id in the command line "
+  value = google_compute_instance.myvm-label.instance_id
+}
 
-# output "vm-external-ip" {
-#   description = "to get public ip in the CMD"
-#   value = google_compute_instance.myvm-label.network_interface.0.access_config.0.nat_ip
+output "vm-external-ip" {
+  description = "to get public ip in the CMD"
+  value = google_compute_instance.myvm-label.network_interface.0.access_config.0.nat_ip
 
-# }
+}
 
-# output "resource-self-link-url" {
-#   description = "After created resource we can get resource address location by url"
-#   value = google_compute_instance.myvm-label.self_link
-# }
+output "resource-self-link-url" {
+  description = "After created resource we can get resource address location by url"
+  value = google_compute_instance.myvm-label.self_link
+}
